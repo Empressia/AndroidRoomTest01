d1d8ae22d21fa4890e9a0d82a684be3ccad0f6a9
すふぃあ <sphere.soffie@gmail.com>
2023-02-25 08:22:13 +0900

    列であることがわかりやすいようにColumnInfoを追加しました。

--- app/src/main/java/jp/empressia/androidroomtest01/Test01.java
+++ app/src/main/java/jp/empressia/androidroomtest01/Test01.java
@@ -1,5 +1,6 @@
 package jp.empressia.androidroomtest01;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
@@ -9,6 +10,7 @@ public class Test01 {
 	@PrimaryKey
 	public int ID;
 
+	@ColumnInfo(name=ColumnInfo.INHERIT_FIELD_NAME)
 	public TEST TEST;
 
 	public static enum TEST {
